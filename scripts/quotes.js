22177646e30ceb599130a72e2ccbe19bb2611c21
Chaitanya Allu <Chaitanya40@users.noreply.github.com>
2016-06-25 01:42:21 +0530

    Create quotes.js

---
+++ scripts/quotes.js
@@ -0,0 +1,54 @@
+var quotes = [
+{
+	"quote":"Don't cry because it's over, smile because it happened.",
+	"author":"Dr. Seuss",
+	"color":"#16a085"
+},
+{
+	"quote":"Be yourself; everyone else is already taken",
+	"author":"Oscar Wilde",
+	"color":"#27ae60"
+},
+{
+	"quote":"Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+	"author":"Albert Einstein",
+	"color":"#2c3e50"
+},
+{
+	"quote":"Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind",
+	"author":"Bernard M. Baruch",
+	"color":"#f39c12"
+},
+{
+	"quote":"A room without books is like a body without a soul",
+	"author":"Marcus Tullius Cicero",
+	"color":"#e74c3c"	
+},
+{
+	"quote":"So many books, so little time.",
+	"author":"Frank Zappa",
+	"color":"#342224"	
+},
+{
+	"quote":"You only live once, but if you do it right, once is enough",
+	"author":" Mae West",
+	"color":"#FB6964"	
+},
+{
+	"quote":"Be the change that you wish to see in the world.",
+	"author":"Mahatma Gandhi",
+	"color":"#9b59b6"	
+},
+{
+	"quote":"In three words I can sum up everything I've learned about life: it goes on.",
+	"author":"Robert Frost",
+	"color":"#FB6964"	
+},
+{
+	"quote":"If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals.",
+	"author":"J.K. Rowling",
+	"color":"#342224"	
+},
+];
+
+
